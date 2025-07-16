c87841651fb646c2cdc9de1c31fc28f817112734
Nehemia Telusma <telusma.nehemia@gmail.com>
2025-07-16 23:40:35 +0200

    feat: delete card added to list + msg when there no card

--- src/components/cards/ListView.tsx
+++ src/components/cards/ListView.tsx
@@ -1,13 +1,17 @@
 import { Card } from "@/types/card"
 import { motion } from "motion/react"
+import DeleteIcon from "../svg/delete"
+import { useCards } from "@/context/CardsContext"
 
 type ListViewProps = {
   cards: Card[]
 }
 
 const ListView = ({ cards }: ListViewProps) => {
+  const { removeCard } = useCards()
+
   return (
-    <section className="px-6 pt-20 bg-zinc-950 min-h-screen">
+    <section className="px-6 pt-20 bg-zinc-900 min-h-screen">
       <motion.header
         initial={{ opacity: 0 }}
         animate={{
@@ -21,6 +25,7 @@ const ListView = ({ cards }: ListViewProps) => {
         <h1 className="font-bold text-4xl text-white">All Cards</h1>
       </motion.header>
 
+    {cards && cards.length > 0 ? (
       <motion.ul
         className="bg-zinc-900 rounded-2xl px-6 pt-3 mt-6"
         initial={{ opacity: 0 }}
@@ -33,12 +38,30 @@ const ListView = ({ cards }: ListViewProps) => {
           transition: { duration: 0.3, ease: "easeOut" }
         }}>
         {cards.map((card) => (
-          <li key={card.id} className="flex flex-col gap-2 py-3 text-sm border-b-[0.5px] border-zinc-800">
-            <p className="font-medium text-white">{card.text}</p>
-            <strong className="text-xs font-light text-zinc-400">{card.createdAt}</strong>
+          <li
+            key={card.id}
+            className="flex justify-between align-middle gap-2 py-3 text-sm border-b-[0.5px] border-zinc-800"
+          >
+            <div className="flex flex-col">
+              <p className="font-medium text-white">{card.text}</p>
+              <strong className="text-xs font-light text-zinc-400">
+                {card.createdAt}
+              </strong>
+            </div>
+            <button onClick={() => removeCard(card.id)}>
+              <DeleteIcon />
+              <span className="sr-only">Delete</span>
+            </button>
           </li>
         ))}
       </motion.ul>
+    ) : (
+      <p className="text-sm text-zinc-500 mt-6">Aucune carte pour le moment.</p>
+    )}
+
+
+
+
     </section>
   )
 }
