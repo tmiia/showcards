6dff6216a01600540e90467040739002b2ebce1b
Nehemia Telusma <telusma.nehemia@gmail.com>
2025-07-16 22:05:44 +0200

    fix: error on build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,53 +1,16 @@
-import js from '@eslint/js';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import a11y from 'eslint-plugin-jsx-a11y';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  js.configs.recommended,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-    },
-  },
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-  {
-    files: ['**/*.jsx', '**/*.tsx'],
-    languageOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    plugins: {
-      react: reactPlugin,
-      'react-hooks': reactHooks,
-      'jsx-a11y': a11y,
-    },
-    rules: {
-      ...reactPlugin.configs.recommended.rules,
-      ...a11y.configs.recommended.rules,
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+export default eslintConfig;
