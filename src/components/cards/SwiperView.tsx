11e564d459a9a9c64adeeb886e6beb242b36df5d
Nehemia Telusma <telusma.nehemia@gmail.com>
2025-07-16 23:40:57 +0200

    feat: card with empty msg added

--- src/components/cards/SwiperView.tsx
+++ src/components/cards/SwiperView.tsx
@@ -23,31 +23,54 @@ const SwiperView = ({ cards, viewState }: SwiperViewProps) => {
           ease: "easeInOut",
         }}
       >
+
         <Swiper
           effect={'cards'}
           grabCursor={true}
           modules={[EffectCards]}
           className="mySwiper z-30 h-screen p-6! max-h-screen overflow-hidden!"
         >
-          {cards.map((card, index) => (
-            <SwiperSlide key={index} className="relative justify-between bg-zinc-900 rounded-4xl p-6">
-              <motion.p
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{
-                duration: 0.5,
-                ease: "easeInOut",
-              }}
-              exit={{
-                opacity: 0,
-                transition: { duration: 0.3, ease: "easeInOut" }
-              }}
-                className="absolute top-[50%] left-[50%] translate-[-50%] font-medium text-white text-7xl text-center w-fit rotate-[90deg]"
-              >
-                {card.text}
-              </motion.p>
-            </SwiperSlide>
-          ))}
+          {cards && cards.length > 0 ? (
+            <>
+             {cards.map((card, index) => (
+              <SwiperSlide key={index} className="relative justify-between bg-zinc-900 rounded-4xl p-6">
+                <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 0.5,
+                  ease: "easeInOut",
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.3, ease: "easeInOut" }
+                }}
+                  className="absolute top-[50%] left-[50%] translate-[-50%] font-medium text-white text-7xl text-center w-fit rotate-[90deg]"
+                >
+                  {card.text}
+                </motion.p>
+              </SwiperSlide>
+            ))}
+            </>
+            ) : (
+              <SwiperSlide className="relative justify-between bg-zinc-900 rounded-4xl p-6">
+                <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 0.5,
+                  ease: "easeInOut",
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.3, ease: "easeInOut" }
+                }}
+                  className="absolute top-[50%] left-[50%] translate-[-50%] font-medium text-white text-lg text-center w-fit"
+                >
+                  Aucune carte pour le moment.
+                </motion.p>
+              </SwiperSlide>
+          )}
         </Swiper>
       </motion.div>
     </>
