5f0d7a6118e2433adb9bf6cfc4e98b7b12b40d6c
Nehemia Telusma <telusma.nehemia@gmail.com>
2025-07-16 14:28:19 +0200

    init: setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,49 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import next from '@next/eslint-plugin-next';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config(
+  { ignores: ['dist', '.next'] },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        React: 'writable',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@next/next': next,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    name: 'next/core-web-vitals',
+    rules: {
+      ...next.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    name: 'prettier-overrides',
+    rules: {
+      ...prettierConfig.rules,
+    },
+  }
+);
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,14 @@
 const config = {
+  theme: {
+    extend: {
+      colors: {
+        'dark-grey': '#222222',
+        'white': '#FBFBFF',
+        'primary': '#3448C6',
+        'secondary': '#9CA3D2',
+      },
+    },
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
