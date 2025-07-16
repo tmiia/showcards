62531413f05fa0fe81571ab792489c95347d9041
Nehemia Telusma <telusma.nehemia@gmail.com>
2025-07-16 20:37:33 +0200

    feat: motion added

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,6 +3,7 @@ const config = {
     extend: {
       colors: {
         'dark-grey': '#222222',
+        'medium-grey': '#333232',
         'white': '#FBFBFF',
         'primary': '#3448C6',
         'secondary': '#9CA3D2',
